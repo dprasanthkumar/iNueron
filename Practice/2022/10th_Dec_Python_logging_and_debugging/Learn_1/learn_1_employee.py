217d2c01ccddb0c9bd3f7d530f66f92a594df37e
dprasanthkumar <110323686+dprasanthkumar@users.noreply.github.com>
2023-12-30 22:30:55 +0530

    sdasdas

--- Practice/2022/10th_Dec_Python_logging_and_debugging/Learn_1/learn_1_employee.py
+++ Practice/2022/10th_Dec_Python_logging_and_debugging/Learn_1/learn_1_employee.py
@@ -1,13 +1,13 @@
 import logging
 import learn_1_saveToFile as STF
 
-# below is the code where we are creating our own logger
+# below is the code where we are creating our own loggecd   r
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 f=logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
 
-fh=logging.FileHandler(filename='learn_saveToFile.log')
+fh=logging.FileHandler(filename='adasdasd.log')
 fh.setFormatter(f)
 logger.addHandler(fh)
 
@@ -15,7 +15,7 @@ logger.addHandler(fh)
 
 #logging.basicConfig(filename='learn_saveToFile.log',level=logging.DEBUG,format='%(asctime)s:%(levelname)s:%(message)s')
 
-logging.debug('Start of employee program')
+logger.debug('Start of employee program')
 
 name = 'imran'
 age = 27
@@ -24,8 +24,8 @@ email = 'imran@gmail.com'
 if STF.namecheck(name) is True:
     STF.saveData(name,age,email)
 else:
-    logging.critical('Employee check failed')
+    logger.critical('Employee check failed')
 
 
-logging.debug('End of employee program')
-logging.debug('######################')
+logger.debug('End of employee program')
+logger.debug('######################')
--- Practice/2022/10th_Dec_Python_logging_and_debugging/Learn_1/learn_1_saveToFile.py
+++ Practice/2022/10th_Dec_Python_logging_and_debugging/Learn_1/learn_1_saveToFile.py
@@ -2,27 +2,30 @@
 import os
 import logging
 
-#logger=logging.getLogger(__name__)
-#logger.setLevel(logging.DEBUG)
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-#f=logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
-logging.basicConfig(filename='learn_saveTofile.log',level=logging.DEBUG,format='%(asctime)s:%(levelname)s:%(message)s')
+f=logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 
+fh=logging.FileHandler('adasdasd.log')
+fh.setFormatter(f)
+
+logger.addHandler(fh)
 
 
 #logging.basicConfig(filename='saveTofile.log',level=logging.DEBUG,format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 def namecheck(name):
-    logging.debug(f'checking name "{name}"....')
+    logger.debug(f'checking name "{name}"....')
     if os.path.exists('data.txt'):
         with open('data.txt','r') as readFile:
             for line in readFile:
                 if line.lower().startswith(f'name: {name.lower()}'):
-                    logging.error(f'Name: "{name}" already exists')
+                    logger.error(f'Name: "{name}" already exists')
                     return False
             if len(name) == 0:
-                logging.critical('Name cannot be blank')
+                logger.critical('Name cannot be blank')
                 return False
             elif not name.isalpha():
                 logging.critical('name must be an alphabet')
